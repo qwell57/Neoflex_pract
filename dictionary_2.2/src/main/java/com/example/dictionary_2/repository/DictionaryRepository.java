d24168e00ed10f5617620cc54f01249097b4afdd
qwell57 <30300367+qwell57@users.noreply.github.com>
2024-07-17 01:09:06 +0300

    Delete dictionary_2.2 directory

--- dictionary_2.2/src/main/java/com/example/dictionary_2/repository/DictionaryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.dictionary_2.repository;
-
-import com.example.dictionary_2.model.Entity.DictionaryEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface DictionaryRepository extends JpaRepository<DictionaryEntity, UUID> {
-}
